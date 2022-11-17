9813175b6c3c2ef99146c1b54e17d8b12e85b773
marcela981 <marcela.mazo@correounivalle.edu.co>
2022-11-16 23:04:14 -0500

    Versión1.3.2 Se agregó la idea de un chat al juego

---
+++ src/Chat/SimpleChat.java
@@ -0,0 +1,26 @@
+/*
+  Archivo: Chat.java
+  Autor: Marcela Mazo
+  Email: <marcela.mazo@correounivalle.edu.co>
+  Modificado por: Marcela Mazo
+  <marcela.mazo@correounivalle.edu.co>
+  Fecha creación: 2022-10-16
+  Fecha última modificación: 2022-11-16
+  Versión: 1.3.2
+  Licencia: GPL
+*/
+
+/*
+ CLASE: Chat
+ INTENCIÓN: Modelar un chat en el juego, en el cual jugadores que se encuentren en el lobby puedan hablar
+ -
+ RELACIONES:
+ -
+ -
+ */
+
+package Chat;
+
+public class SimpleChat
+{
+}
