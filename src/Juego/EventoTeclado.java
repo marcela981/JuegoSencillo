d53f1aa11d44c66b3bd4deeb0b8f9e5c4a918945
marcela981 <marcela.mazo@correounivalle.edu.co>
2022-11-30 11:55:36 -0500

    Versión 2.0 Reforma total del juego.
    Se modificó de forma limpia el juego.
    
    La V2.0 Cuenta con dos jugadores de forma local y sonido

---
+++ src/Juego/EventoTeclado.java
@@ -0,0 +1,69 @@
+/*
+  Archivo: Jugador.java
+  Autor: Marcela Mazo, Hassen Ortiz
+  Email: <marcela.mazo@correounivalle.edu.co> <hasse.ortiz@correounivalle.edu.co>
+  Modificado por: Marcela Mazo, Hassen Ortiz
+  <marcela.mazo@correounivalle.edu.co>
+  Fecha creación: 2022-10-16
+  Fecha última modificación: 2022-11-30
+  Versión: 2.0
+  Licencia: GPL
+*/
+
+/*
+ CLASE: EventoTeclado
+ INTENCIÓN: Interaccion con las flechas del teclado y el programa que se está corriendo,
+ La idea es que de forma simultanea dos partes del teclado interactuen con el programa
+ - Se puede mover con flechas del teclado
+  - Se puede mover con las teclas W,A,S,D
+ */
+
+
+package Juego;
+
+import java.awt.event.*;
+
+
+public class EventoTeclado extends KeyAdapter {
+
+    static boolean w, s, up, down;
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int id = e.getKeyCode();
+        if (id == KeyEvent.VK_W) {
+            w = true;
+
+        }
+        if (id == KeyEvent.VK_S) {
+            s = true;
+
+        }
+        if (id == KeyEvent.VK_UP) {
+            up = true;
+        }
+        if (id == KeyEvent.VK_DOWN) {
+            down = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+        int id = e.getKeyCode();
+        if (id == KeyEvent.VK_W) {
+            w = false;
+        }
+        if (id == KeyEvent.VK_S) {
+            s = false;
+        }
+        if (id == KeyEvent.VK_UP) {
+            up = false;
+        }
+        if (id == KeyEvent.VK_DOWN) {
+            down = false;
+        }
+    }
+
+}
---
+++ src/Recursos/Audio.java
@@ -0,0 +1,36 @@
+/*
+  Archivo: Audio.java
+  Autor: Marcela Mazo, Hassen Ortiz
+  Email: <marcela.mazo@correounivalle.edu.co> <hasse.ortiz@correounivalle.edu.co>
+  Modificado por: Marcela Mazo, Hassen Ortiz
+  <marcela.mazo@correounivalle.edu.co>
+  Fecha creación: 2022-10-16
+  Fecha última modificación: 2022-11-30
+  Versión: 1.0
+  Licencia: GPL
+*/
+
+/*
+ CLASE: Audio
+ INTENCIÓN: Modelamos el audio que interactua con el ambiente del juego
+ Cada rebote con las raquetas sonará, cada falta sonará.
+ Este se moverá atra vez del tablero del juego y será el medio del juego
+ - Interactua con la pelota y la raqueta
+
+ RELACIONES:
+ -Conoce el sonido
+ */
+
+package Recursos;
+
+import java.applet.AudioClip;
+import Sonido.*;
+
+public class Audio {
+
+    public AudioClip getAudio(String direccion) {
+        return java.applet.Applet.newAudioClip(getClass().getResource(direccion));
+    }
+
+}
+
